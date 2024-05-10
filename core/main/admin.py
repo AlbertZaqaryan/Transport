d76e00903d8d6da2bdf125a81bd89df4bb649169
AlbertZaqaryan <itm.albert.zaqaryan@gmail.com>
2024-05-10 09:40:24 +0400

    test_v1

---
+++ core/main/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import Logo, Info, Background,About, Track, Reviws, ContactUs
+# Register your models here.
+
+admin.site.register(Logo)
+admin.site.register(Info)
+admin.site.register(Background)
+admin.site.register(About)
+admin.site.register(Track)
+admin.site.register(Reviws)
+
+@admin.register(ContactUs)
+class ContactAdmin(admin.ModelAdmin):
+
+    list_display = ['id', 'name', 'email']
+    list_display_links = ['id', 'name']
+    search_fields = ['name', 'email']
